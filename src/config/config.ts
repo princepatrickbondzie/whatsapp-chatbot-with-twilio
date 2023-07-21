d6fffc834ae55cf1313c6dfed912023e71080ad2
Prince Bondzie <bondzieprince@gmail.com>
2023-07-21 13:18:17 +0000

    update response message

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,8 +6,4 @@ export const Configs = {
   MONGODB_URL: process.env.MONGODB_URL,
   TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
-  API_KEY: process.env["API_KEY"],
-  BASE_URL: "https://api-football-v1.p.rapidapi",
-  X_RAPIDAPI_HOST: "api-football-v1.p",
-  RAPIDAPI_KEY: process.env["X_RAPIDAPI_KEY"],
 };
