7442ad1b1c02f9873c938a50c9f80a153d808eae
Prince Bondzie <bondzieprince@gmail.com>
2023-07-21 12:48:10 +0000

    remove middleware

--- src/routes/bot/schema.ts
+++ src/routes/bot/schema.ts
@@ -2,8 +2,9 @@ import Joi from "@hapi/joi";
 
 export default {
   payload: Joi.object().keys({
-    name: Joi.string().required(),
-    message: Joi.string().required(),
-    from: Joi.string().required(),
+    ProfileName: Joi.string().required(),
+    Body: Joi.string().required(),
+    WaId: Joi.string().required(),
+    From: Joi.string().required(),
   }),
 };
